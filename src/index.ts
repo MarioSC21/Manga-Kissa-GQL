1ec17a4edfda03e27035db8336f16f0f9e465614
MarioSC21 <mariocamna@gmail.com>
2022-09-17 18:35:41 -0500

    config depoly server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import { ApolloServer } from 'apollo-server'
+import config from './config'
 import { typeDefs, resolvers } from './graphql/schema/schema'
+import {
+  ApolloServerPluginCacheControl,
+  ApolloServerPluginLandingPageLocalDefault
+} from 'apollo-server-core'
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginCacheControl({
+      // Cache everything for 1 second by default.
+      defaultMaxAge: 1,
+      // Don't send the `cache-control` response header.
+      calculateHttpHeaders: false
+    }),
+    ApolloServerPluginLandingPageLocalDefault({ embed: true })
+  ]
 })
 
-void server.listen().then(({ url }) => {
+void server.listen(config.port).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
